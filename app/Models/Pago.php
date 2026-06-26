7f220f4e241d577fbf3584fb73cc782dc495c963
“Alejandro <licenses@plusvalica.com>
2026-06-26 14:14:04 -0600

    fix: agregar fillable al modelo Pago

--- app/Models/Pago.php
+++ app/Models/Pago.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pago extends Model
 {
-    //
+    protected $fillable = [
+        'agencia_id', 'suscripcion_id', 'monto', 'metodo',
+        'stripe_payment_intent_id', 'referencia', 'status', 'fecha_pago',
+    ];
 }
